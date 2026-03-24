51de5592f95ec0c30f998e60bc9ee3e5b27f4458
erickhogarth <xandesteamgames977@gmail.com>
2026-03-23 23:25:37 -0300

    feat(api): add TypeORM with MySQL connection #2

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '3306'),
+      username: process.env.DB_USERNAME ?? 'root',
+      password: process.env.DB_PASSWORD ?? 'root',
+      database: process.env.DB_DATABASE ?? 'benefits_ai',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
